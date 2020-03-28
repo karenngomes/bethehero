5aeb5114c3fd8c762cf7eefb33cd049166c45709
Karen Gomes <kngs@ic.ufal.br>
2020-03-28 10:53:57 -0300

    params type

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
+app.use(express.json());
+
+/*
+ * Tipos de parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Exemplo: '/users?page=2&name=Karen&idade=21
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Exemplo: '/users/:id
+ * Request Body: corpo da requisição, utilizado para criar ou alterar recursos
+ *
+ */
+
+app.post("/users", (request, response) => {
+  const params = request.body;
+  console.log(params);
   return response.json({
     event: "Semana OmniStack 11.0",
     aluna: "Karen Gomes"
