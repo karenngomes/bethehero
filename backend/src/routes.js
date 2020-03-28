9868569bf8122ac58b30737461837ee6e2788520
Karen Gomes <kngs@ic.ufal.br>
2020-03-28 12:05:15 -0300

    post method to create an ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const crypto = require("crypto");
+
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.post("/users", (request, response) => {
-  const params = request.body;
-  console.log(params);
-  return response.json({
-    event: "Semana OmniStack 11.0",
-    aluna: "Karen Gomes"
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
   });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
