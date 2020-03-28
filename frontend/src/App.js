5dedd2abf96d20ec4b337d25900b97c6771d0a6e
Karen Gomes <kngs@ic.ufal.br>
2020-03-28 15:27:08 -0300

    props concept

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello, World</h1>;
+  return <Header>Semana Omnistack</Header>;
 }
 
 export default App;
