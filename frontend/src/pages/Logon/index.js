c00d635518860d22ee44f8124c6829861eced745
Karen Gomes <kngs@ic.ufal.br>
2020-03-28 16:01:53 -0300

    create login page structure

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-import './styles.css'
+import "./styles.css";
+
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 
 export default function Logon() {
-    return (
-        <h1>Logon page</h1>
-    )
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+          <button type="submit">Entrar</button>
+
+          <a href="/register">Não tenho cadastro</a>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
