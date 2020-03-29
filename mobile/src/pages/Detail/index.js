c5d07f21188ea5274448dfe729f0163532a41aab
Karen Gomes <kngs@ic.ufal.br>
2020-03-29 15:13:52 -0300

    dynamic incident detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { View, TouchableOpacity, Image, Text, Linking } from "react-native";
 import * as MailComposer from "expo-mail-composer";
 
@@ -9,9 +9,16 @@ import styles from "./styles";
 
 export default function Detail() {
   const navigation = useNavigation();
-  const message =
-    "Olá APAE, estou entrando em contto pois gostaria de ajudar no caso 'Gatinho atropelado' com o valor de R$ 120,00";
-  const phone = "+558299999999"; // must be a real phone
+
+  const route = useRoute();
+  const incident = route.params.incident;
+
+  const formattedValue = Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(incident.value);
+
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso '${incident.title}' com o valor de ${formattedValue}`;
 
   function navigateBack() {
     navigation.goBack();
@@ -19,14 +26,16 @@ export default function Detail() {
 
   function sendMail() {
     MailComposer.composeAsync({
-      subject: "Herói do caso: Gatinho atropelado",
-      recipients: ["kngs@ic.ufal.br"],
+      subject: `Herói do caso: ${incident.title}`,
+      recipients: [incident.email],
       body: message
     });
   }
 
   function sendWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=${phone}&text=${message}`);
+    Linking.openURL(
+      `whatsapp://send?phone=${incident.whatsapp}&text=${message}`
+    );
   }
 
   return (
@@ -40,13 +49,15 @@ export default function Detail() {
 
       <View style={styles.incident}>
         <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-        <Text style={styles.incidentValue}>APAE</Text>
+        <Text style={styles.incidentValue}>
+          {incident.name} de {incident.city}/{incident.uf}
+        </Text>
 
         <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>Gatinho atropelado</Text>
+        <Text style={styles.incidentValue}>{incident.title}</Text>
 
         <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>R$ 120,00</Text>
+        <Text style={styles.incidentValue}>{formattedValue}</Text>
       </View>
 
       <View style={styles.contactBox}>
